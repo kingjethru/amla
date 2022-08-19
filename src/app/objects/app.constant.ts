a44e61b168ae3d55dea50fafa58bd6e62b8c1f2b
marxbal <marxbal@mapfreinsurance.com.ph>
2022-08-19 19:50:30 +0800

    Update app.constant.ts

--- src/app/objects/app.constant.ts
+++ src/app/objects/app.constant.ts
@@ -1,7 +1,7 @@
 export const VERSION = '1.20220819';
 export const CONTACT_HOTLINE = '(02) 8876-4400';
-export const CONTACT_GLOBE = '(+63) 920-918-6242';
-export const CONTACT_SMART = '(+63) 917-581-7175';
+export const CONTACT_SMART = '(+63) 920-918-6242';
+export const CONTACT_GLOBE = '(+63) 917-581-7175';
 export const CONTACT_EMAIL = 'teleservice@mapfreinsular.com';
 export const SOC_MED_FACEBOOK = '//facebook.com/mapfreph';
 export const SOC_MED_TWITTER = '//twitter.com/mapfreph';
