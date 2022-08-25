5c4640781cb53bed8034dcce1785a802508eaecd
marxbal <marxbal@mapfreinsurance.com.ph>
2022-08-26 00:47:50 +0800

    fix

--- src/app/component/upload/otp/otp.component.ts
+++ src/app/component/upload/otp/otp.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { CONTACT_EMAIL } from 'src/app/objects/app.constant';
 
 @Component({
   selector: 'app-otp',
@@ -8,8 +9,13 @@ import { FormGroup } from '@angular/forms';
 })
 export class OtpComponent implements OnInit {
   @Input() formGroup: FormGroup = new FormGroup({});
+  @Input() clientName: string = '';
+  @Input() clientEmail: string = '';
+
   @Output() resendOTP: EventEmitter<any> = new EventEmitter();
   @Output() verifyOTP: EventEmitter<any> = new EventEmitter();
+
+  contactEmail: string = CONTACT_EMAIL;
 
   constructor() {}
 
